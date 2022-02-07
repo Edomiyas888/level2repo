984f04f8ad388b6ea5255b2cec2436bde180b01a
edomiyas <edomias888@gmail.com>
2022-02-07 09:52:46 -1000

    hello is updated again 2

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,4 @@
 this is master at finding inttelegent attitude
 this is the master from the internet via github
 this is the third master at finding intellegence attitude
+this is edited just after the branch is createds
